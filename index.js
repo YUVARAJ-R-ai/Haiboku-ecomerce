97fec15d3d2763d6f4c9aaf6cf3c8c27bb5c4617
YUVARAJ-R-ai <yuvaraj28022005@gmail.com>
2024-09-17 20:23:23 +0530

    second commit

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ let price={
     "hoodie":800
 }
 
-let quantity={}
+let quantity={
+    "shirt":0,
+    "tshirt":0,
+    "hoodie":0
+}
 const price_cal=(type)=>{
+    let cart=document.getElementById("total")
     if(type in quantity){
         quantity[type]+=1
     }
@@ -17,8 +22,25 @@ const price_cal=(type)=>{
     for(let i in quantity){
         tot_price=tot_price+quantity[i]*price[i]
     }
-    console.log(tot_price)
+    // cart.innerHTML
     
+    cart.innerHTML=`shirt:${quantity["shirt"]}<br>tshirt:${quantity["tshirt"]}<br>hoodie:${quantity["hoodie"]}<br>Total:${tot_price}`
 }
 
-
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
